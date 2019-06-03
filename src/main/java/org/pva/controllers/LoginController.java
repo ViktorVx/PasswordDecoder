30bd6c749a70586383db2deb8fe1cfad7aa28b0d
pva <pva@mail.ru>
2019-06-03 11:39:24 +0400

    Add file-path and password validation

---
+++ src/main/java/org/pva/controllers/LoginController.java
@@ -0,0 +1,68 @@
+package org.pva.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LoginController {
+
+    @FXML
+    TextField filePathField;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    public void selectFileOnClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("TXT", "*.txt"));
+        File file = fileChooser.showOpenDialog(new Stage());
+        if (file!=null)
+            filePathField.setText(file.getAbsolutePath());
+    }
+
+    @FXML
+    public void decryptBtnOnClick() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Data entry error");
+        if (filePathField == null || filePathField.getText() == null || filePathField.getText().equals("")) {
+            alert.setHeaderText("Empty file path");
+            alert.setContentText("Please, choose file path");
+            alert.show();
+            return;
+        }
+
+        if (passwordField == null || passwordField.getText() == null || passwordField.getText().equals("")) {
+            alert.setHeaderText("Empty password");
+            alert.setContentText("Please, enter password");
+            alert.show();
+            return;
+        }
+
+        char[] pass = null;
+        if (passwordField != null) {
+            pass = passwordField.getText().toCharArray();
+        }
+    }
+
+    @FXML
+    public void createNewBtnOnClick() throws IOException {
+        Stage passwordsList = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/passwordsList.fxml"));
+        passwordsList.setTitle("Passwords");
+        passwordsList.setScene(new Scene(root, 400, 400));
+        passwordsList.show();
+    }
+
+
+
+}
