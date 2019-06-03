87ebea404d8bc37701bd63cc35dba25ade26c3db
pva <pva@mail.ru>
2019-06-03 10:14:53 +0400

    Add file chooser

--- src/main/java/org/pva/controllers/Controller.java
+++ src/main/java/org/pva/controllers/Controller.java
@@ -1,6 +1,25 @@
 package org.pva.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class Controller {
+
+    @FXML
+    TextField filePathField;
+
+    @FXML
+    public void selectFileOnClick() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("TXT", "*.txt"));
+        File file = fileChooser.showOpenDialog(new Stage());
+        if (file!=null)
+            filePathField.setText(file.getAbsolutePath());
+    }
 
 
 
